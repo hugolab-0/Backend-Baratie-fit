57b533f9b0d0deba6969b0e918205370ad950850
Geovane.Santos <25203771@CFP127ALUNO.LOCAL>
2026-06-18 08:21:22 -0300

    teste

--- API - ADM/controller/modulo/config_upload_azure.js
+++ API - ADM/controller/modulo/config_upload_azure.js
@@ -1,6 +1,6 @@
 //Criação das contantes de configuração da AZURE
 
-const TOKEN       = 'sp=racw&st=2026-06-18T02:48:00Z&se=2026-06-18T10:00:00Z&sv=2026-02-06&sr=c&sig=9wV7M6%2BZrWrve7tV%2BYQ0Lfi%2F%2Ff6bNEflypLb1%2F22yLI%3D'
+const TOKEN       = 'sp=racw&st=2026-06-18T10:57:43Z&se=2026-06-18T19:12:43Z&spr=https&sv=2026-02-06&sr=c&sig=5qFPpfjzEFCQiqg1TLufKeGX9qwcx9Nes45zVYggdEo%3D'
 const ACCOUNT     = 'uploadrefeicao'
 const CONTAINER   = 'uploadrefeicao'
 
