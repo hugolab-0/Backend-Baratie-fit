5262a10a9630ef1e1d5a6a5b759b2582447f8194
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-06-19 09:24:35 -0300

    correcao antes da apresentacao

--- API - User/model/database_config_knex/knexFile.js
+++ API - User/model/database_config_knex/knexFile.js
@@ -5,7 +5,7 @@ module.exports = {
     connection: {
       host: 'localhost',
       user: 'root', // Substitua pelo seu usuário
-      password: 'Go139bra!', // Substitua pela sua senha
+      password: 'bcd127', // Substitua pela sua senha
       database: 'db_baratiefit_2029', // Substitua pelo nome do seu banco de dados
       port: 3306, // Porta padrão do MySQL
      
